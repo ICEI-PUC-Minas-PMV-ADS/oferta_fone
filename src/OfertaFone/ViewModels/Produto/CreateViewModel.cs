f89a3361406049147c0fd8888d7a127310ec2160
Ursula Figueiredo <ursula_daniela@hotmail.com>
2022-10-11 15:06:25 -0300

    Criação de produto

--- src/OfertaFone/ViewModels/Produto/CreateViewModel.cs
+++ src/OfertaFone/ViewModels/Produto/CreateViewModel.cs
@@ -1,12 +1,11 @@
 ﻿using OfertaFone.WebUI.ViewModels.Base;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 
 namespace OfertaFone.WebUI.ViewModels.Produto
 {
-    public class IndexViewModel : BaseViewModel
+    public class CreateViewModel : BaseViewModel
     {
         public string Nome { get; set; }
+
         public double Preco { get; set; }
     }
 }
