aca2292571dfdd76e770b9f3e9fd758113bf6ae5
pedrovitorrs <pedro442.rodrigues@gmail.com>
2022-10-11 16:55:37 -0300

    feat: corrigindo bug usuario navbar. #30

---
+++ src/OfertaFone/ViewComponents/LayoutNavBarViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using OfertaFone.WebUI.Identity;
+using OfertaFone.WebUI.ViewModels.Components;
+using System.Threading.Tasks;
+
+namespace OfertaFone.WebUI.ViewComponents
+{
+    [ViewComponent(Name = "LayoutNavBarViewComponent")]
+    public class LayoutNaveBarViewComponent : ViewComponent
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public LayoutNaveBarViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var appUser = await _userManager.FindByNameAsync(
+                HttpContext.User.Identity.Name);
+
+            var vm = new LayoutNavBarViewModel
+            {
+                UserName = appUser?.UserName,
+                Email = appUser?.Email,
+            };
+
+            return View("../Shared/_LayoutNavBar.cshtml", vm);
+        }
+    }
+}
---
+++ src/OfertaFone/ViewModels/Components/LayoutNavBarViewModel.cs
@@ -0,0 +1,15 @@
+﻿using OfertaFone.WebUI.ViewModels.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OfertaFone.WebUI.ViewModels.Components
+{
+    public class LayoutNavBarViewModel : BaseViewModel
+    {
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public bool Enabled { get; set; }
+    }
+}
