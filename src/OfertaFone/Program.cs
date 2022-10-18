a5f2cd5357b2f1b4625f15d06f48f3cfb47f0c7f
pedrovitorrs <pedro442.rodrigues@gmail.com>
2022-10-18 18:07:31 -0300

    feat: deploy. #30

--- src/OfertaFone/Program.cs
+++ src/OfertaFone/Program.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Azure.Identity;
 
 namespace OfertaFone
 {
@@ -19,11 +18,6 @@ namespace OfertaFone
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-.ConfigureAppConfiguration((context, config) =>
-{
-var keyVaultEndpoint = new Uri(Environment.GetEnvironmentVariable("VaultUri"));
-config.AddAzureKeyVault(keyVaultEndpoint, new DefaultAzureCredential());
-})
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
