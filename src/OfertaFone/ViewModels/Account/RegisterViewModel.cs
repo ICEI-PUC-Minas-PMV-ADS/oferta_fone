ce8e642870b325532edbdecbb42f5dfbbb8dc583
pedrovitorrs <pedro442.rodrigues@gmail.com>
2022-10-18 17:35:18 -0300

    feat: inserindo deploy aplicação.#30

--- src/OfertaFone/ViewModels/Account/RegisterViewModel.cs
+++ src/OfertaFone/ViewModels/Account/RegisterViewModel.cs
@@ -10,6 +10,8 @@ namespace OfertaFone.WebUI.ViewModels.Account
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(20, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         [Required]
         [EmailAddress]
