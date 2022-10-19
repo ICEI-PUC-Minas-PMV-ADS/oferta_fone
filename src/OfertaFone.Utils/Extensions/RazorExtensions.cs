29c65ea59f42ce1697ea8112c2618e55e907fbff
pedrovitorrs <pedro442.rodrigues@gmail.com>
2022-10-19 16:02:33 -0300

    feat: inserindo componente carrinho de compras. #30

--- src/OfertaFone.Utils/Extensions/RazorExtensions.cs
+++ src/OfertaFone.Utils/Extensions/RazorExtensions.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,11 +10,40 @@ namespace OfertaFone.Utils.Extensions
 {
     public static class RazorExtensions
     {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="email"></param>
+        /// <returns></returns>
+        public static string HashForGravatar(this RazorPage page, string chave)
+        {
+            var md5Hasher = MD5.Create();
+            var data = md5Hasher.ComputeHash(Encoding.Default.GetBytes(chave));
+            var sBuilder = new StringBuilder();
+            foreach (var t in data)
+            {
+                sBuilder.Append(t.ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="page"></param>
+        /// <param name="valor"></param>
+        /// <returns></returns>
         public static string FormatMoney(this RazorPage page, decimal valor)
         {
             return FormatMoney(valor);
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="valor"></param>
+        /// <returns></returns>
         private static string FormatMoney(decimal valor)
         {
             return string.Format(System.Globalization.CultureInfo.GetCultureInfo("pt-BR"), "{0:C}", valor);
